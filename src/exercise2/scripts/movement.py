b7164222bf81398f9e99bea0f51856d797c9ceff
BlazBone <bone.blaz@gmail.com>
2023-03-09 09:59:39 +0100

    movements complete, split server into server and publisher

--- src/exercise2/scripts/movement.py
+++ src/exercise2/scripts/movement.py
@@ -1,35 +1,70 @@
 #!/usr/bin/python3
 
-import rospy as rp
+import rospy
 from geometry_msgs.msg import Twist
+import random
 
 def rectangle_movement(step):
 
   twist = Twist()
-  twist.linear.x = 0.1
-  step = step % 80
+  twist.linear.x = 1
+  step = step % 20
 
-  if step % 20 == 0:
+  if step % 5 == 0:
     twist.linear.x = 0
     twist.angular.z = 1.57 #(90 / 360) * 2 * 3.14
 
   return twist
 
+
+def triangle_movement(step):
+
+  twist = Twist()
+  twist.linear.x = 1
+  step = step % 9
+
+  if step % 3 == 0:
+    twist.linear.x = 0
+    twist.angular.z = (120 / 360) * 2 * 3.14
+
+  return twist
+
+
+def circle_movement(step):
+
+  twist = Twist()
+  twist.linear.x = 1
+  twist.angular.z = (70 / 360) * 2 * 3.14
+
+  return twist
+
+
+def random_movement(step):
+
+  twist = Twist()
+  twist.linear.x = rospy.get_param("~scale_linear") * random.random()
+  twist.angular.z = rospy.get_param('~scale_angular') * 2 * (random.random() - 0.5) # +-
+
+  return twist
+
 def movement():
 
-  pub = rp.Publisher('cmd_vel', Twist, queue_size = 1000)
+  pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1000)
   # For the turtle simulation map the topic to /turtle1/cmd_vel
   # For the turtlebot simulation and Turtlebot map the topic to /cmd_vel_mux/input/navi
-  rp.init_node('movement')
+  rospy.init_node('movement')
 
-  r = rp.Rate(10000)
+  r = rospy.Rate(2)
 
   step = 0.0
-  
-  while not rp.is_shutdown():
-    twist = rectangle_movement(step)
+
+  while not rospy.is_shutdown():
+    # twist = rectangle_movement(step)
+    # twist = triangle_movement(step)
+    # twist = circle_movement(step)
+    twist = random_movement(step)
     pub.publish(twist)
-    step = step + 2
+    step = step + 1.0
     r.sleep()
 
 if __name__ == '__main__':
--- src/exercise2/scripts/movement_service.py
+++ src/exercise2/scripts/movement_service.py
@@ -3,40 +3,84 @@
 import rospy as rp
 from geometry_msgs.msg import Twist
 import random
-
+import time
 from exercise2.srv import CommandTest,CommandTestResponse
+
+def rectangle_movement(step):
+
+  twist = Twist()
+  twist.linear.x = 0.5
+  step = step % 20
+
+  if step % 5 == 0:
+    twist.linear.x = 0
+    twist.angular.z = 1.57 #(90 / 360) * 2 * 3.14
+
+  return twist
+
+
+def triangle_movement(step):
+
+  twist = Twist()
+  twist.linear.x = 1
+  step = step % 9
+
+  if step % 3 == 0:
+    twist.linear.x = 0
+    twist.angular.z = (120 / 360) * 2 * 3.14
+
+  return twist
+
+
+def circle_movement(step):
+
+  twist = Twist()
+  twist.linear.x = 1
+  twist.angular.z = (70 / 360) * 2 * 3.14
+
+  return twist
+
+
+def random_movement(step):
+
+  twist = Twist()
+  twist.linear.x = rp.get_param("~scale_linear") * random.random()
+  twist.angular.z = rp.get_param('~scale_angular') * 2 * (random.random() - 0.5) # +-
+
+  return twist
+
 
 def handle_reqeust(req):
     print("Got", req.type)
     movement_type = req.type
     duration = req.duration
 
-    pub = rp.Publisher("/turtle1/cmd_vel", Twist, queue_size=100)
+    pub = rp.Publisher("cmd_vel", Twist, queue_size=100)
+
+    time_end = time.time() + duration
+    step = 0
+
+    
 
     if movement_type == "circle":
-        twitst = Twist()
-        twitst.linear.x = 1
-        twitst.angular.z = 1
+        function = circle_movement
     elif movement_type == "rectangle":
-        twitst = Twist()
-        twitst.linear.x = 1
-        twitst.angular.z = 0
+        function = rectangle_movement
     elif movement_type == "triangle":
-        twitst = Twist()
-        twitst.linear.x = 1
-        twitst.angular.z = 0.5
+        function = triangle_movement
     elif movement_type == "random":
-        twitst = Twist()
-        twitst.linear.x = random.randint(0, 10)
-        twitst.angular.z = random.randint(0, 10)
+        function = random_movement
     else:
         rp.logerr("Unknown movement type: %s", movement_type)
         return CommandTestResponse("Unknown movement type: %s" % movement_type)
     
-
+    rp.loginfo("Starting movement: %s", movement_type)
     start_time = rp.Time.now()
-    while rp.Time.now() - start_time < rp.Duration(duration):
-        pub.publish(twitst)
+    # write a loop that runs for 'duration' seconds
+    while rp.Time.now() < start_time + rp.Duration(duration):
+        twist = function(step)
+        pub.publish(twist)
+        step = step + 1.0
         rp.sleep(0.1)
 
     stop_twist = Twist()
@@ -49,6 +93,7 @@ def handle_reqeust(req):
 
 
 def movement_server():
+    rp.rate(2)
     rp.init_node("movement_server")
     s = rp.Service("movement_service", CommandTest, handler=handle_reqeust)
     print("Ready to move the turtle")
